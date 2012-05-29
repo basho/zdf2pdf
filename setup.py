f456e30375912afbbede8777d6b6a68c70cfcd7b
Brent Woodruff <bwoodruff@basho.com>
2012-05-29 11:03:21 -0400

    Restructure as a module

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = "0.1"
+
+setup(name="zdfversion",
+      version=version,
+      description="Make PDFs out of Zendesk forums.",
+      long_description="Make PDFs out of Zendesk forums.",
+      classifiers=["Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Documentation",
+      ],
+      keywords="zendesk pdf",
+      author="Brian Shumate",
+      author_email="bshumate@basho.com",
+      url="http://www.basho.com",
+      license="Apache",
+      packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+      include_package_data=True,
+      zip_safe=False,
+      requires=['pycurl==7.19.0',
+               ]
+)
---
+++ zdfversion/__init__.py
@@ -0,0 +1,4 @@
+# See zdfversion.zdf for detailed documentation
+#
+# Written by Brian Shumate <bshumate@basho.com>
+#        and Brent Woodruff <bwoodruff@basho.com>
