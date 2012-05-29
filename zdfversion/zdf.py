6b520ec235fe53f7ec764dbd793fe205aa577cf8
Brent Woodruff <bwoodruff@basho.com>
2012-05-29 18:18:41 -0400

    remove some unused code and comments

--- zdfversion/zdf.py
+++ zdfversion/zdf.py
@@ -8,11 +8,9 @@ zdfversion: Helper script for Zendesk forum entry (knowledge base)
 
 class ZDF:
     def __init__(self, creds, url):
-        self.creds = creds # e.g. "you@example.com/token:dneib393fwEF3ifbsEXAMPLEdhb93dw343"
+        # creds looks like you@example.com/token:dneib393fwEF3ifbsEXAMPLEdhb93dw343
+        self.creds = creds
         self.url = url + '/api/v1/forums/'
-        #self.entries = "entries.xml"
-        #self.forums = "forums.xml"
-        #self.post_base = "https://help.basho.com/api/v1/"
 
     def curl(self, forum_id):
         """Wrapper function around PyCurl with XML/Zd specific bits"""
@@ -33,8 +31,6 @@ class ZDF:
         curl.setopt(curl.MAXREDIRS, 5)
         curl.setopt(curl.USERPWD, self.creds)
         curl.setopt(curl.CUSTOMREQUEST, "GET")
-        #if not data == "null":
-        #    curl.setopt(curl.POSTFIELDS, data)
         curl.perform()
         result = sio.getvalue()
         sio.close()
@@ -53,38 +49,6 @@ class ZDF:
             elem.clear()
         for elem in tree.iterfind('entry/updated-at'):
             elem.clear()
-
-# <FIXME> This might not be the approach now
-"""
-if args.forum_id:
-    entries = zdf_curl(zdf_get_base + args.forum_id + '/' + zdf_entries, "GET", "null")
-    tmpfile = "./entries_tmp.xml"
-    with open(tmpfile, "w") as outfile:
-        outfile.write(entries)
-    tree = et.ElementTree(file=tmpfile)
-    zdf_strip()
-    if args.output_file:
-        clnfile = args.output_file
-    else:
-        clnfile = "./entries_clean.xml"
-    with open(clnfile, "w") as newfile:
-        tree.write(newfile)
-    os.unlink(tmpfile)
-else:
-    print "I need a forum identifier to continue."
-    sys.exit(1)
-
-if entries.filename:
-    if entries.newforum:
-        print "Gonna make a new forum, YAY!"
-        # <TODO> create forum xml
-        # <TODO> post forum xml
-        # <TODO> return newly created forum's id
-
-    else:
-        print "You must specify a new forum name in quotes"
-        sys.exit(1)
-"""
 
 def main(argv=None):
     import os, sys, argparse
