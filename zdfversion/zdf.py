6c25848308400d2e6260bc7e5db9093151b67e5d
Brent Woodruff <bwoodruff@basho.com>
2012-05-29 14:02:57 -0400

    Unfinished OOP refactor

--- zdfversion/zdf.py
+++ zdfversion/zdf.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 zdfversion: Helper script for Zendesk forum entry (knowledge base)
             documentation set version creation and maintenance.
@@ -8,7 +6,6 @@ zdfversion: Helper script for Zendesk forum entry (knowledge base)
             Example <forum_id>: 20767107
 """
 
-import argparse
 import os
 import pycurl
 import sys
@@ -21,80 +18,46 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-creds = "you@example.com/token:dneib393fwEF3ifbsEXAMPLEdhb93dw343"
-zdf_entries = "entries.xml"
-zdf_forums = "forums.xml"
-zdf_get_base = "https://help.basho.com/api/v1/forums/"
-zdf_post_base = "https://help.basho.com/api/v1/"
-
-parser = argparse.ArgumentParser(description="Gets current versions and posts new versions of Zendesk forum entries")
-parser.add_argument('-f', action='store', dest='entries_filename',
-                    help='Specify XML file containing Zendesk forum entries for uploading')
-
-parser.add_argument('-i', action='store', dest='forum_id',
-                    help='Specify an 8 digit Zendesk forum identifier from which to get entries')
-parser.add_argument('-n', action='store', dest='new_forum', help='Specify the quoted name of the new forum target for documentation snapshot')
-parser.add_argument('-o', action='store', dest='output_file',
-                    help='Specify optional output filename for forum entries file (default: entries_clean.xml)')
-# <TODO>
-# parser.add_argument("-v","--verbose", help="Verbose output")
-# args = parser.parse_args(['-h'])
-args = parser.parse_args()
-
-def zdf_curl(fid, verb, data):
-    """Wrapper function around PyCurl with XML/Zd specific bits"""
-    b = cStringIO.StringIO()
-    c = pycurl.Curl()
-    if verb == "GET":
-        c.setopt(c.HTTPHEADER, ['Accept: application/xml'])
-        zdf_url = zdf_get_base + args.forum_id + '/' + zdf_entries
-    else:
-        c.setopt(c.HTTPHEADER, ['Content-type: application/xml'])
-        zdf_url = zdf_post_base + zdf_entries
-    c.setopt(c.URL, zdf_url)
-    c.setopt(c.WRITEFUNCTION, b.write)
-    c.setopt(c.CONNECTTIMEOUT, 5)
-    c.setopt(c.TIMEOUT, 7)
-    c.setopt(c.FOLLOWLOCATION, True)
-    c.setopt(c.MAXREDIRS, 5)
-    c.setopt(c.USERPWD, creds)
-    c.setopt(c.CUSTOMREQUEST, verb)
-    if not data == "null":
-        c.setopt(c.POSTFIELDS, data)
-    c.perform()
-    result = b.getvalue()
-    b.close()
-    return result;
 
 
-# <TODO> clean this up a bit
-def zdf_strip():
-    """Strips elements which need to be regenerated automatically by the API at POST time"""
-    for elem in tree.iterfind('entry/created-at'):
-        elem.clear()
-    for elem in tree.iterfind('entry/id'):
-        elem.clear()
-    for elem in tree.iterfind('entry/submitter-id'):
-        elem.clear()
-    for elem in tree.iterfind('entry/updated-at'):
-        elem.clear()
+class ZDF():
+    def __init__(creds, url):
+        self.creds = creds # e.g. "you@example.com/token:dneib393fwEF3ifbsEXAMPLEdhb93dw343"
+        self.url = url + '/api/v1/forums/'
+        self.entries = "entries.xml"
+        #self.forums = "forums.xml"
+        #self.post_base = "https://help.basho.com/api/v1/"
 
+    def curl(forum_id):
+        """Wrapper function around PyCurl with XML/Zd specific bits"""
+        sio = cStringIO.StringIO()
+        curl = pycurl.Curl()
+        curl.setopt(curl.HTTPHEADER, ['Accept: application/xml'])
+        curl.setopt(curl.URL, self.url + forum_id + '/' + self.entries)
+        curl.setopt(curl.WRITEFUNCTION, sio.write)
+        curl.setopt(curl.CONNECTTIMEOUT, 5)
+        curl.setopt(curl.TIMEOUT, 7)
+        curl.setopt(curl.FOLLOWLOCATION, True)
+        curl.setopt(curl.MAXREDIRS, 5)
+        curl.setopt(curl.USERPWD, self.creds)
+        curl.setopt(curl.CUSTOMREQUEST, "GET")
+        #if not data == "null":
+        #    curl.setopt(curl.POSTFIELDS, data)
+        curl.perform()
+        result = sio.getvalue()
+        sio.close()
+        return result;
 
-def zdf_new_forum(nfname):
-    root = ET.Element('forum')
-    forum_name = ET.SubElement(root, 'name')
-    forum_name.text = nfname
-    restrict = ET.SubElement(root, 'visibility-restriction-id')
-    restrict.text = "3"
-    tree = ET.ElementTree(root)
-    #<TODO> API call and catch result for new forum id
-
-def zdf_post_new(nfid, data):
-    # testing
-    nfid = "20846071"
-    new_entries = zdf_curl(zdf_get_base + args.forum_id + '/' + zdf_entries, "GET", "null")
-    tree = ET.ElementTree(new_entries)
-    zdfstrip()
+    def strip():
+        """Strips elements which need to be regenerated automatically by the API at POST time"""
+        for elem in tree.iterfind('entry/created-at'):
+            elem.clear()
+        for elem in tree.iterfind('entry/id'):
+            elem.clear()
+        for elem in tree.iterfind('entry/submitter-id'):
+            elem.clear()
+        for elem in tree.iterfind('entry/updated-at'):
+            elem.clear()
 
 # <FIXME> This might not be the approach now
 """
@@ -127,3 +90,58 @@ if entries.filename:
         print "You must specify a new forum name in quotes"
         sys.exit(1)
 """
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+    import argparse
+
+    args = argparse.ArgumentParser(
+        description='Make a PDF from a Zendesk forum.')
+    args.add_argument('-c', action='store', dest='config_filename',
+        default='~/.zdfversion.cfg',
+        help='Zendesk configuration filename (default: ~/.zdfversion.cfg)')
+    args.add_argument('-f', action='store', dest='entries_filename',
+        help='Zendesk entries XML file to convert to PDF')
+    args.add_argument('-i', action='store', dest='forum_id',
+        help='Zendesk forum ID to download and convert to PDF')
+    args.add_argument('-o', action='store', dest='pdf_filename',
+        help='PDF output filename')
+    args.add_argument('-t', action='store', dest='pdf_title',
+        help='PDF title')
+    args.add_argument("-v","--verbose",
+        help="Verbose output")
+    args.add_argument("-h","--help",
+        help="Help and Usage")
+
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            args = args.parse_args()
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+             raise Usage(msg)
+        # more code, unchanged
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+    import ConfigParser as configparser
+    config = configparser.RawConfigParser()
+    config.read(config_filename)
+    email = config.get('zdfversion', 'email')
+    token = config.get('zdfversion', 'token')
+    url   = config.get('zdfversion', 'url')
+
+    creds = email + '/token:' + token
+    ZDF(creds = "you@example.com/token:dneib393fwEF3ifbsEXAMPLEdhb93dw343",
+          url = url
+       )
+
+if __name__ == "__main__":
+    main()
+
