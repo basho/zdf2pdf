42bf0c40cb5bcabdb8eb8992d1146c7875f1f460
Brent Woodruff <bwoodruff@basho.com>
2012-05-30 10:28:35 -0400

    Remove pycurl version from setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(name="zdfversion",
       packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
       include_package_data=True,
       zip_safe=False,
-      requires=['pycurl==7.19.0',
+      requires=['pycurl',
                ]
 )
