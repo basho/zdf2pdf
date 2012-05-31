a4e7612f9b82759494fe1960e2c78a3707222243
Brent Woodruff <bwoodruff@basho.com>
2012-05-31 15:32:37 -0400

    Switch to zendesk module. Forum listing is working again

--- zdf2pdf/zdf2pdf.py
+++ zdf2pdf/zdf2pdf.py
@@ -50,18 +50,6 @@ class zdf2pdf:
         sio.close()
         return result;
 
-    def read_config(self, config_file):
-        # Read zendesk info from config file
-        import ConfigParser as configparser
-        config = configparser.RawConfigParser()
-        config.read(config_file)
-        email = config.get('zdf2pdf', 'email')
-        token = config.get('zdf2pdf', 'token')
-        url   = config.get('zdf2pdf', 'url')
-
-        self.creds = email + '/token:' + token
-        self.url   = url
-
 
     def get_all_forums(self):
         return self._zdf_request(self.url + '/forums.xml')
@@ -102,18 +90,38 @@ class zdf2pdf:
         #if pdf.err:
         #    dumpErrors(pdf)
 
-def _config_errmsg():
-    from textwrap import dedent
-    msg = dedent("""\
-        Error: Could not read settings from {config}
+def config_zendesk(config_file):
+    """Read zendesk info from config file"""
+    from zendesk import Zendesk
+    import ConfigParser as configparser
 
-        Expected config file to be of the format:
-        [zdf2pdf]
-        email = you@example.com
-        token = dneib393fwEF3ifbsEXAMPLEdhb93dw343
-        url = https://example.zendesk.com
-        """)
-    print(msg.format(config=args.config_file))
+    config = configparser.RawConfigParser()
+    config.read(config_file)
+    try:
+        email = config.get('zdf2pdf', 'email')
+        token = config.get('zdf2pdf', 'token')
+        url   = config.get('zdf2pdf', 'url')
+
+        # Set up the zendesk object
+        zd = Zendesk(url, 
+                    zendesk_username = email,
+                    zendesk_password = token,
+                    use_api_token = True)
+        return zd
+
+    except configparser.NoSectionError:
+        from textwrap import dedent
+        msg = dedent("""\
+            Error: Could not read settings from {config}
+
+            Expected config file to be of the format:
+            [zdf2pdf]
+            email = you@example.com
+            token = dneib393fwEF3ifbsEXAMPLEdhb93dw343
+            url = https://example.zendesk.com
+            """)
+        print(msg.format(config=config_file))
+        return None
 
 def main(argv=None):
     import os, sys, argparse
@@ -150,10 +158,7 @@ def main(argv=None):
         argv = sys.argv
     args = argp.parse_args()
 
-    # Set up the zdf2pdf object and obtain the xml tree
-    zdf = zdf2pdf()
-
-    # use an xml file on disk
+    # use an entries file on disk
     if args.entries_file:
         # Refrain from guessing about the PDF title when using an entries file
         if not args.pdf_title:
@@ -162,40 +167,38 @@ def main(argv=None):
             return 1
 
         # Get the entries off disk and make the etree
-        tree = et.ElementTree(file=args.entries_file)
+        #tree = et.ElementTree(file=args.entries_file)
 
-    # Get the xml from zendesk
+    # Get the entries from zendesk
     elif args.forum_id:
-        try:
-            zdf.read_config(args.config_file)
-        except configparser.NoSectionError:
-            _config_errmsg()
+        zd = config_zendesk(args.config_file)
+        if not zd:
             return 1
 
         # If no title given, use the forum title from Zendesk
         if not args.pdf_title:
-            forum_tree = et.XML(zdf.get_forum(args.forum_id))
+            forum_tree = zd.show_forum(args.forum_id)
             args.pdf_title = forum_tree.find('name').text
 
         # Get the entries and make the etree
-        entries = zdf.get_forum_entries(args.forum_id)
+        entries = zd.list_entries(args.forum_id)
         tree = et.XML(entries)
 
-        # If requested, save the downloaded XML file
+        # If requested, save the downloaded entries file
         if args.keep_file:
             with open(args.keep_file, "w") as outfile:
                 outfile.write(entries)
     elif args.list_forums:
         # list available zendesk forums with their IDs and exit
-        try:
-            zdf.read_config(args.config_file)
-        except configparser.NoSectionError:
-            _config_errmsg()
+        zd = config_zendesk(args.config_file)
+        if not zd:
             return 1
 
-        tree = et.XML(zdf.get_all_forums())
-        for forum in tree.iter('forum'):
-            print(forum.find('id').text + ' ' + forum.find('name').text)
+        forums = zd.list_forums()
+        #print(forums)
+        #return 1
+        for forum in forums:
+            print(str(forum['id']) + ' ' + forum['name'])
         return 0
     else:
         from textwrap import dedent
